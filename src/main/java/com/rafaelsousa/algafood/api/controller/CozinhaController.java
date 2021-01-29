6d45c26e9147163886daf4b753ddd167a604ca9e
rafaelsous <rafaelsousa.souza@gmail.com>
2021-01-29 00:03:36 -0300

    Kitchen singleton resource.

--- src/main/java/com/rafaelsousa/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/rafaelsousa/algafood/api/controller/CozinhaController.java
@@ -5,6 +5,7 @@ import com.rafaelsousa.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,14 +19,12 @@ public class CozinhaController {
     private CozinhaRepository cozinhaRepository;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Cozinha> listar1() {
-        System.out.println("Listar 1");
+    public List<Cozinha> listar() {
         return cozinhaRepository.listar();
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-    public List<Cozinha> listar2() {
-        System.out.println("Listar 2");
-        return cozinhaRepository.listar();
+    @GetMapping("/{id}")
+    public Cozinha buscar(@PathVariable Long id) {
+        return cozinhaRepository.buscar(id);
     }
 }
