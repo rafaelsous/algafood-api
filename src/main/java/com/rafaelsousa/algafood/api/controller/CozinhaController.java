8561973e18e8044f6bf9f628cb654238fecf54a8
rafaelsous <rafaelsousa.souza@gmail.com>
2021-01-28 23:30:10 -0300

    Kitchen collection resource.

---
+++ src/main/java/com/rafaelsousa/algafood/api/controller/CozinhaController.java
@@ -0,0 +1,24 @@
+package com.rafaelsousa.algafood.api.controller;
+
+import com.rafaelsousa.algafood.domain.model.Cozinha;
+import com.rafaelsousa.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping
+    public List<Cozinha> listar() {
+        return cozinhaRepository.listar();
+    }
+
+}
