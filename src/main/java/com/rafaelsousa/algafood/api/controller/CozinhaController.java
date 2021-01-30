9d0fe0f308d81d81909b8c0612df4ce6fc5eee2a
rafaelsous <rafaelsousa.souza@gmail.com>
2021-01-29 23:16:16 -0300

    Inclusion of resource with POST method.

--- src/main/java/com/rafaelsousa/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/rafaelsousa/algafood/api/controller/CozinhaController.java
@@ -4,20 +4,16 @@ import com.rafaelsousa.algafood.api.controller.model.CozinhasXmlWrapper;
 import com.rafaelsousa.algafood.domain.model.Cozinha;
 import com.rafaelsousa.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
 
 @RestController
-@RequestMapping(path = "/cozinhas")
+@RequestMapping("/cozinhas")
 public class CozinhaController {
 
     @Autowired
@@ -37,17 +33,15 @@ public class CozinhaController {
     public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
         Cozinha cozinha = cozinhaRepository.buscar(id);
 
-//        if (Objects.nonNull(cozinha)) {
-//            return ResponseEntity.ok(cozinha);
-//        }
+        if (Objects.nonNull(cozinha)) {
+            return ResponseEntity.ok(cozinha);
+        }
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.LOCATION, "http://api.algafood.local:8080/cozinhas");
+        return ResponseEntity.noContent().build();
+    }
 
-//        return ResponseEntity.noContent().build();
-        return ResponseEntity
-                .status(HttpStatus.FOUND)
-                .headers(headers)
-                .build();
+    @PostMapping
+    public ResponseEntity<Cozinha> adicionar(@RequestBody Cozinha cozinha) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(cozinhaRepository.salvar(cozinha));
     }
 }
