ac6398995a9aa2751ff53e32d59ea7cadea27feb
rafaelsous <rafaelsousa.souza@gmail.com>
2021-01-29 23:50:25 -0300

    Exclusion of resource with DELETE method.

--- src/main/java/com/rafaelsousa/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/rafaelsousa/algafood/api/controller/CozinhaController.java
@@ -5,6 +5,7 @@ import com.rafaelsousa.algafood.domain.model.Cozinha;
 import com.rafaelsousa.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -58,5 +59,19 @@ public class CozinhaController {
         }
 
         return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Cozinha> remover(@PathVariable Long id) {
+        try {
+            cozinhaRepository.remover(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception ex) {
+            if (ex instanceof DataIntegrityViolationException) {
+                return ResponseEntity.status(HttpStatus.CONFLICT).build();
+            }
+
+            return ResponseEntity.notFound().build();
+        }
     }
 }
