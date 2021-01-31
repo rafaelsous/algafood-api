00c31bb81b524ddc71705579fe82ab0e38a4abfd
rafaelsous <rafaelsousa.souza@gmail.com>
2021-01-31 18:10:00 -0300

    JPQL queries in a XML file.

--- src/main/java/com/rafaelsousa/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/rafaelsousa/algafood/domain/repository/RestauranteRepository.java
@@ -2,7 +2,6 @@ package com.rafaelsousa.algafood.domain.repository;
 
 import com.rafaelsousa.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
@@ -13,7 +12,7 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
 
     List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 
-    @Query(value = "FROM Restaurante res WHERE nome LIKE %:nome% AND res.cozinha.id = :cozinhaId")
+//    @Query(value = "FROM Restaurante res WHERE nome LIKE %:nome% AND res.cozinha.id = :cozinhaId")
     List<Restaurante> buscarPorNomeECozinhaId(String nome, Long cozinhaId);
 
 //    List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
