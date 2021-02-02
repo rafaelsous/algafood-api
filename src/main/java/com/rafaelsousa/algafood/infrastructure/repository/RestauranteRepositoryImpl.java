c4f71a777a1e76289206c202bd7811a1ae20e669
rafaelsous <rafaelsousa.souza@gmail.com>
2021-02-01 23:03:37 -0300

    Search with Criteria API.

--- src/main/java/com/rafaelsousa/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/rafaelsousa/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -3,11 +3,19 @@ package com.rafaelsousa.algafood.infrastructure.repository;
 import com.rafaelsousa.algafood.domain.model.Restaurante;
 import com.rafaelsousa.algafood.domain.repository.RestauranteRepositoryQueries;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Repository
 public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
@@ -17,13 +25,29 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 
     @Override
     public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
-        var jpql = "FROM Restaurante res WHERE res.nome LIKE :nome " +
-                "AND res.taxaFrete BETWEEN :taxaFreteInicial AND :taxaFreteFinal";
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
 
-        return manager.createQuery(jpql, Restaurante.class)
-                .setParameter("nome", "%" + nome + "%")
-                .setParameter("taxaFreteInicial", taxaFreteInicial)
-                .setParameter("taxaFreteFinal", taxaFreteFinal)
-                .getResultList();
+        CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+        Root<Restaurante> root = criteria.from(Restaurante.class);
+
+        var predicates = new ArrayList<Predicate>();
+
+        if (StringUtils.hasText(nome)) {
+            predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
+        }
+
+        if (Objects.nonNull(taxaFreteInicial)) {
+            predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
+        }
+
+        if (Objects.nonNull(taxaFreteFinal)) {
+            predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
+        }
+
+        criteria.where(predicates.toArray(new Predicate[0]));
+
+        TypedQuery<Restaurante> query = manager.createQuery(criteria);
+
+        return query.getResultList();
     }
 }
