0d9f273ce235307bd3d7f9f513d1ca86fbb009d8
rafaelsous <rafaelsousa.souza@gmail.com>
2021-05-28 16:38:42 -0300

    Test to validate request body.

--- src/test/java/com/rafaelsousa/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/rafaelsousa/algafood/CadastroCozinhaIT.java
@@ -1,11 +1,15 @@
 package com.rafaelsousa.algafood;
 
-import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+
+import static io.restassured.RestAssured.enableLoggingOfRequestAndResponseIfValidationFails;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CadastroCozinhaIT {
@@ -15,15 +19,30 @@ public class CadastroCozinhaIT {
 
     @Test
     public void testarRetorno200QuandoConsultarCozinhas() {
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        enableLoggingOfRequestAndResponseIfValidationFails();
 
-        RestAssured.given()
-                .basePath("/cozinhas")
-                .port(port)
-                .accept(ContentType.JSON)
-                .when()
-                .get()
-                .then()
-                .statusCode(HttpStatus.OK.value());
+        given()
+            .basePath("/cozinhas")
+            .port(port)
+            .accept(ContentType.JSON)
+        .when()
+            .get()
+        .then()
+            .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    public void testarRetornoDe4ResultadosQuandoConsultarCozinhas() {
+        enableLoggingOfRequestAndResponseIfValidationFails();
+
+        given()
+            .basePath("/cozinhas")
+            .port(port)
+            .accept(ContentType.JSON)
+        .when()
+            .get()
+        .then()
+            .body("", hasSize(4))
+            .body("nome", hasItems("Tailandesa", "Indiana"));
     }
 }
